ec83665e21aadcbf89e1b7dc24206d5a1f9dfb9a
dekwx <jakub.prochaazka@gmail.com>
2025-12-02 11:22:55 +0100

    test.py

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ number = 0
 while number < 100:
     number = number + 1  # Add 1 to the current number
     # Optional: print the number at each step
-    # print(number) 
+print(number) 
 
 # This line runs after the loop finishes (when number is 100)
 print(f"The final number is: {number}")
+
+# i want to add this somehow to my bot like if you ask it to count to 100. Right now i really don't know how to make it useful but it is a cool script ig ;)
